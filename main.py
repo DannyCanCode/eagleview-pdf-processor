dbf3a302d53440bcfd1dd575bfd04a943f1494cb
DannyCanCode <danielpedraza@me.com>
2025-02-12 14:42:25 -0500

    Handle PORT env variable in main.py

--- main.py
+++ main.py
@@ -112,5 +112,9 @@ async def test_pdf(file: UploadFile):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    # Get port from environment variable with fallback to 8000
+    try:
+        port = int(os.environ.get("PORT", "8000"))
+    except ValueError:
+        port = 8000
     uvicorn.run(app, host="0.0.0.0", port=port) 
